a21f0fba00d33f4700d277a3af6611a1a4b3d778
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2020-10-19 19:13:56 -0500

    backend

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -7,7 +7,7 @@
 """
 Define and annotate variables with the specified values.
 """
-a = int(1)
-pi = float(3.14)
-i_understand_annotations = bool(True)
-school = str('Holberton')
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = 'Holberton'
