90738c3976b258ac31b678e00df63fcc6f829bd6
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2020-10-20 22:38:36 -0500

    task

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+3. Tasks
+"""
+import asyncio
+from time import time
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int):
+    """
+    function that measures the total execution time
+    """
+    return asyncio.create_task(wait_random())
