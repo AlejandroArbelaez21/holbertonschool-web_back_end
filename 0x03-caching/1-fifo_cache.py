fa6d554c17e6b4306fa93ced16f3fbcd6da20df6
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2020-10-26 18:40:59 -0500

    FIFO caching

---
+++ 0x03-caching/1-fifo_cache.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" BasicCache module
+"""
+
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """ FIFOCache defines two functions:
+      - put
+      - get
+    """
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        """ assign to the dictionary the item value for the key key """
+        if key and item:
+            self.cache_data[key] = item
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                list_k = []
+                for k in self.cache_data.keys():
+                    list_k.append(k)
+                print("DISCARD: {}".format(list_k[0]))
+                del self.cache_data[list_k[0]]
+
+    def get(self, key):
+        """ return the value in self.cache_data linked to key """
+        return self.cache_data.get(key)
