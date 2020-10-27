81363b8a556ed42d0e721795474a59a8cbd74735
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2020-10-26 21:56:46 -0500

    FIFO caching

--- 0x03-caching/1-fifo_cache.py
+++ 0x03-caching/1-fifo_cache.py
@@ -13,16 +13,17 @@ class FIFOCache(BaseCaching):
     def __init__(self):
         """ Constructor """
         super().__init__()
+        self.list_k = []
 
     def put(self, key, item):
         """ assign to the dictionary the item value for the key key """
         if key and item:
             self.cache_data[key] = item
             if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                for k in self.cache_data:
-                    del self.cache_data[k]
-                    print("DISCARD: {}".format(k))
-                    break
+                sorted_dict_keys = sorted(self.cache_data)
+                first = sorted_dict_keys[0]
+                del self.cache_data[first]
+                print('DISCARD: {}'.format(first))
 
     def get(self, key):
         """ return the value in self.cache_data linked to key """
