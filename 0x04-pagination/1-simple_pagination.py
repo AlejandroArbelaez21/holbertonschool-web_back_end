9dbdb47b2563c793a1b8e7a4c1798adf07356924
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2020-10-28 16:03:11 -0500

    Simple pagination

--- 0x04-pagination/1-simple_pagination.py
+++ 0x04-pagination/1-simple_pagination.py
@@ -41,6 +41,7 @@ class Server:
         page with default value 1 and page_size with default value 10.
         """
         assert type(page) == int and type(page_size) == int
+        assert type(page) == int or type(page_size) == int
         assert page > 0 and page_size > 0
         self.dataset()
         index = index_range(page, page_size)
