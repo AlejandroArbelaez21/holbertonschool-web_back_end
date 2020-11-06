d1cc1f2f672dc6a0b5515a86d9803daa0094160a
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2020-11-06 15:33:14 -0500

    Basic - User credentials

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,8 @@
 6. Basic auth
 """
 from api.v1.auth.auth import Auth
+import base64
+import binascii
 
 
 class BasicAuth(Auth):
@@ -19,3 +21,33 @@ class BasicAuth(Auth):
                 return authorization_header[6:]
             else:
                 return None
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                           str) -> str:
+        """ returns the decoded value of a Base64 string """
+        if base64_authorization_header is None:
+            return None
+        if isinstance(base64_authorization_header, str):
+            try:
+                base64_authorization_header = base64.b64decode(
+                                            base64_authorization_header
+                                          )
+                return base64_authorization_header.decode('utf-8')
+            except Exception:
+                return None
+
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header:
+                                 str) -> (str, str):
+        """ returns the user email and password from Base64 decoded value """
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if isinstance(decoded_base64_authorization_header, str):
+            if ":" in decoded_base64_authorization_header:
+                validate = decoded_base64_authorization_header.split(":", 1)
+                return validate[0], validate[1]
+            else:
+                return None, None
+        else:
+            return None, None
