503a60cf8998a0623d3be81b67bf45007a48be2d
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2020-11-06 14:42:34 -0500

    Basic auth

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 
 """
-6. Basic auth 
+6. Basic auth
 """
 from api.v1.auth.auth import Auth
 
 
 class BasicAuth(Auth):
     """ class BasicAuth that inherits from Auth """
-    
