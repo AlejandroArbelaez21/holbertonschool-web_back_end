137fe6b444fa819f038ee6a2423c8255ead774ac
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2020-11-11 17:34:43 -0500

    Et moi et moi et moi!

---
+++ 0x07-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+"""
+3. Auth class
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """ Class for authentication """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ check paths in excluded_paths """
+        if path is None:
+            return True
+        if excluded_paths is None or not excluded_paths:
+            return True
+        if path[-1] != '/':
+            path += '/'
+        for excluded in excluded_paths:
+            if excluded[-1] == '*':
+                if path[0] == excluded[:1]:
+                    return False
+        return False if path in excluded_paths else True
+
+    def authorization_header(self, request=None) -> str:
+        """ Flask request object """
+        if request is None:
+            return None
+        if "Authorization" not in request.headers:
+            return None
+        return request.headers["Authorization"]
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Flask request object """
+        return None
