8693a9e31d500ca2970ee88c41972841cb4a8483
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2020-11-29 16:19:32 -0500

    Basic Flask app

--- 0x0A-i18n/0-app.py
+++ 0x0A-i18n/0-app.py
@@ -2,18 +2,18 @@
 """
 Basic Flask app
 """
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    """ GET route /
+    """route /
     Return:
       - a welcome message
     """
-    return "Hello World!"
+    return render_template("0-index.html")
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0", port="5000")
