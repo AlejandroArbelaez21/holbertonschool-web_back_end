e9aa97efd154d545e22ef6de8057c3c35b0f9664
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2020-11-29 16:15:06 -0500

    Basic Flask app

--- 0x0A-i18n/0-app.py
+++ 0x0A-i18n/0-app.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python3
+"""
+Basic Flask app
+"""
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
+    """ GET route /
+    Return:
+      - a welcome message
+    """
     return "Hello World!"
+
 
 if __name__ == '__main__':
     app.run()
