4ca234909fd6e2bf815b5a23b6e7be844c4cb163
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2020-12-01 17:43:52 -0500

    Retrieving lists

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -2,20 +2,82 @@
 """
 Writing strings to Redis
 """
+from typing import Union, Callable, Optional
 from redis import Redis
 from uuid import uuid1
-from typing import Union
+import sys
+from functools import wraps
 
 
-class Cache():
+def count_calls(method: Callable) -> Callable:
+    """ return function that increments the count for that key every time """
+    key = method.__qualname__
+
+    @wraps(method)
+    def wrapper(self, *args, **kwds):
+        """ wrapper function """
+        self._redis.incr(key)
+        return method(self, *args, **kwds)
+    return wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    """ store the history of inputs and outputs for a particular function."""
+    key = method.__qualname__
+
+    @wraps(method)
+    def wrapper(self, *args):
+        """ wrapper function """
+        self._redis.rpush("{}:inputs".format(key), str(args))
+        result = method(self, *args)
+        self._redis.rpush("{}:outputs".format(key),
+                          str(result))
+        return result
+    return wrapper
+
+
+def replay(method: Callable):
+    """ Displays nthe history of calls """
+    r = method.__self__._redis
+    method_name = method.__qualname__
+
+    inputs = r.lrange("{}:inputs".format(method_name), 0, -1)
+    outputs = r.lrange("{}:outputs".format(method_name), 0, -1)
+
+    print("{} was called {} times:".format(method_name,
+          r.get(method_name).decode("utf-8")))
+    for i, o in tuple(zip(inputs, outputs)):
+        print("{}(*('{}',)) -> {}".format(method_name, i.decode("utf-8"),
+              o.decode("utf-8")))
+
+
+class Cache:
     """ Cache class """
+
     def __init__(self):
         """ store an instance of the Redis client as a private variable """
         self._redis = Redis()
         self._redis.flushdb()
 
+    @call_history
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """ store the input data in Redis using the random key and return """
-        random = str(uuid1())
-        self._redis.set(random, data)
-        return random
+        key = str(uuid1())
+        self._redis.set(key, data)
+        return key
+
+    def get(self,
+            key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """  Reading from Redis and recovering original type """
+        res = self._redis.get(key)
+        return fn(res) if fn else res
+
+    def get_str(self, data: bytes) -> str:
+        """ Converts bytes to string. """
+        return data.decode('utf-8')
+
+    def get_int(self, data: bytes) -> int:
+        """ Converts bytes to int. """
+        return int.from_bytes(data, sys.byteorder)
