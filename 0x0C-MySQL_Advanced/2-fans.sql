c95e6f493a6839a60b886dd86b687fbfd80a3f94
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2020-12-04 17:48:47 -0500

    Old school band

--- 0x0C-MySQL_Advanced/2-fans.sql
+++ 0x0C-MySQL_Advanced/2-fans.sql
@@ -1,3 +1,3 @@
 -- 3 first students in the Batch ID=3
 -- because Batch 3 is the best!
-SELECT origin, sum(fans) AS nb_fans FROM metal_bands GROUP BY origin ORDER BY nb_fans desc;
+SELECT origin, sum(fans) AS nb_fans FROM metal_bands GROUP BY origin ORDER BY nb_fans DESC;
