d99e473306617deb15cea5f4bbd1f8f3b0cfeb85
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2020-12-04 19:54:03 -0500

    Email validation to sent

--- 0x0C-MySQL_Advanced/5-valid_email.sql
+++ 0x0C-MySQL_Advanced/5-valid_email.sql
@@ -1,9 +1,11 @@
 -- 3 first students in the Batch ID=3
 -- because Batch 3 is the best!
+DELIMITER //
 CREATE TRIGGER email_validation
 BEFORE UPDATE ON users FOR EACH ROW
 BEGIN
 	IF OLD.email <> NEW.email THEN
 	   SET NEW.valid_email = 0;
     END IF;
-END;
+END;//
+DELIMITER ;
