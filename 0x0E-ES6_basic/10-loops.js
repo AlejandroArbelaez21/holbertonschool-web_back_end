397ce42ee33638f26352f69921688c8934415491
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2020-12-12 00:46:44 -0500

    fix eslint

--- 0x0E-ES6_basic/10-loops.js
+++ 0x0E-ES6_basic/10-loops.js
@@ -1,9 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
-    const tempArray = array;
-    for (const element of array) {
-      const idx = array.indexOf(element);
-      tempArray[idx] = `${appendString}${element}`;
-    }
-  
-    return tempArray;
+  const tempArray = array;
+  for (const element of array) {
+    const idx = array.indexOf(element);
+    tempArray[idx] = `${appendString}${element}`;
   }
+
+  return tempArray;
+}
