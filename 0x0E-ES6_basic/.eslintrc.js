49dd8837a29d3cd49accd6511b237e02d55c0e99
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2020-12-11 21:44:02 -0500

    Block Scope

--- 0x0E-ES6_basic/.eslintrc.js
+++ 0x0E-ES6_basic/.eslintrc.js
@@ -1,22 +1,25 @@
 module.exports = {
     "env": {
         "browser": true,
-        "es6": true
+        "es6": true,
+        "node": true
     },
     "extends": [
         "eslint:recommended",
+        "plugin:vue/essential",
         "plugin:@typescript-eslint/eslint-recommended"
     ],
     "globals": {
         "Atomics": "readonly",
         "SharedArrayBuffer": "readonly"
     },
-    "parser": "@typescript-eslint/parser",
     "parserOptions": {
         "ecmaVersion": 2018,
+        "parser": "@typescript-eslint/parser",
         "sourceType": "module"
     },
     "plugins": [
+        "vue",
         "@typescript-eslint"
     ],
     "rules": {
