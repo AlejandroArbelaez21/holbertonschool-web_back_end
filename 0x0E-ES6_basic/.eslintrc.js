387cea6dac9ff8ee4c0c8df1b5f6dc288b528776
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2020-12-11 23:31:29 -0500

    eslint

--- 0x0E-ES6_basic/.eslintrc.js
+++ 0x0E-ES6_basic/.eslintrc.js
@@ -1,37 +1,20 @@
 module.exports = {
-    env: {
-      browser: false,
-      es6: true,
-      jest: true,
+    "env": {
+        "es6": true,
+        "node": true
     },
-    extends: [
-      'airbnb-base',
-      'plugin:jest/all',
+    "extends": "plugin:vue/essential",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue"
     ],
-    globals: {
-      Atomics: 'readonly',
-      SharedArrayBuffer: 'readonly',
-    },
-    parserOptions: {
-      ecmaVersion: 2018,
-      sourceType: 'module',
-    },
-    plugins: ['jest'],
-    rules: {
-      'no-console': 'off',
-      'no-shadow': 'off',
-      'no-restricted-syntax': [
-        'error',
-        'LabeledStatement',
-        'WithStatement',
-      ],
-    },
-    overrides:[
-      {
-        files: ['*.js'],
-        excludedFiles: 'babel.config.js',
-      }
-    ]
-  };
-  
-  
+    "rules": {
+    }
+};
